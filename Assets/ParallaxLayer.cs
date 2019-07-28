6b16b1f457908511fc330085d29501504920f5ba
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-07-28 22:06:43 +0300

    Unity3D

---
+++ Assets/ParallaxLayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 0.5f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * speed, 0);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+
+}
