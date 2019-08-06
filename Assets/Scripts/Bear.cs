0b416fe3787387b2a37ce6f26f547e94dacb020d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-08-06 14:05:00 +0300

    Better, more performant assetS

---
+++ Assets/Scripts/Bear.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bear : MonoBehaviour
+{
+
+    public float yForce = 500f;
+    private bool isDead = false;
+    private new Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rigidbody.velocity = Vector2.zero;
+                rigidbody.AddForce(new Vector2(0, yForce));
+            }
+        }
+
+    }
+}
