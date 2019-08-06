bb9ea6d1bf4dfcdadef78dcfb2a075c31960e3e4
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-08-06 15:36:39 +0300

    wow

--- Assets/Scripts/Bear.cs
+++ Assets/Scripts/Bear.cs
@@ -8,25 +8,41 @@ public class Bear : MonoBehaviour
     public float yForce = 500f;
     private bool isDead = false;
     private new Rigidbody2D rigidbody;
+    private Animator animator;
+
+    private bool canJump = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (isDead == false)
+        if (isDead == false && canJump == true)
         {
             if (Input.GetMouseButtonDown(0))
             {
+                animator.SetTrigger("Jump");
                 rigidbody.velocity = Vector2.zero;
                 rigidbody.AddForce(new Vector2(0, yForce));
+                canJump = false;
+
             }
         }
 
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+        if (collision.collider.tag == "ground")
+        {
+            canJump = true;
+        }
     }
 }
---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = -0.5f;
+    private new Rigidbody2D rigidbody;
+
+    private BoxCollider2D objectCollider;
+    private float objectWidth;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector2(speed, 0);
+        objectCollider = GetComponent<BoxCollider2D>();
+        objectWidth = objectCollider.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -objectWidth)
+        {
+            Reposition();
+        }
+    }
+
+    void Reposition()
+    {
+        Vector2 groundOffset = new Vector2(objectWidth * 2, 0);
+        transform.position = (Vector2)transform.position + groundOffset;
+    }
+}
